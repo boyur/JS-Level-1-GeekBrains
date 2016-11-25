3760970e7d7be21c0ae7febe7f1a252c54841dc4
Artem Boyur <gboyur@gmail.com>
2016-11-25 19:49:25 +0300

    add practices

--- lesson3/dz3.js
+++ lesson3/dz3.js
@@ -1,18 +1,13 @@
-var size = 10;
-var text = "";
+String.prototype.repeat = function(n) {
+  return new Array(n+1).join(this);
+};
 
-for (var a = 1; a < size; a++) {
-  if (a % 2) {
-    text += '#';
-  } else {
-    text += ' ';
-  }
-}
+var size = 10;
 
 for (var b = 1; b < size; b++) {
   if (b % 2) {
-    console.log(text);
+    console.log('# '.repeat(size/2));
   } else {
-    console.log(' ' + text);
+    console.log(' #'.repeat(size/2));
   }
 }
